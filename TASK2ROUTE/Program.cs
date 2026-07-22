eb50bac9f52b052cc13f9dc9be751913246d0b10
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-07-22 19:54:35 +0300

    project initialization

---
+++ TASK2ROUTE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TASK2ROUTE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
