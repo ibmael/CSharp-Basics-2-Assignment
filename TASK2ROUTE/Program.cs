c860196cbbbae071f526c26c58ac9c1ccee62d46
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-07-22 21:09:02 +0300

    Task 1: Logical AND operator

--- TASK2ROUTE/Program.cs
+++ TASK2ROUTE/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            //Task One
+            #region Task 1
+            int pages1 = 464;
+            bool isAvailable = true;
+            if (pages1 > 300 && isAvailable)
+            {
+                Console.WriteLine("Task 1: You can borrow this book");
+            }
+            Console.WriteLine();
+            #endregion
         }
     }
 }
